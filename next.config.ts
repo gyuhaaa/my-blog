4ef9a00635379f8bdc9f772b9798f06979bc8e61
gyuhaaa <vkvkvk2892@naver.com>
2025-02-21 17:23:02 +0900

    allow localhost

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,18 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "http",
+        hostname: "localhost", // 로컬호스트 허용
+        port: "3000", // 포트 3000 허용
+        pathname: "/**", // 모든 경로 허용
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1", // 127.0.0.1도 별도로 등록
+        port: "3000",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
         hostname: "192.168.0.6",
         port: "3000",
         pathname: "/**", // 모든 경로 허용
--- src/posts/http-image-2.md
+++ src/posts/http-image-2.md
@@ -1,7 +1,7 @@
 ---
 title: "http image 2"
 date: "2025-02-21"
-thumbnail: "http://192.168.0.6:3000/images/1.jpg"
+thumbnail: "http://localhost:3000/images/1.jpg"
 ---
 
 http 이미지 테스트 2
