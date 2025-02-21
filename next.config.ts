c6e00951f3d5de194148131c5f887c0988146484
gyuhaaa <vkvkvk2892@naver.com>
2025-02-21 16:45:48 +0900

    modify config for http image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "http://192.168.0.6:3000/images/1.jpg", // HTTP 이미지를 제공하는 도메인
+        port: "", // 포트가 있다면 추가
+        pathname: "/**", // 모든 경로 허용
+      },
+    ],
+  },
 };
 
 export default nextConfig;
